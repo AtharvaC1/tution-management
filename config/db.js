08c33b24226430204dc80d88051096713d01b634
AtharvaC1 <atharva.anupama@gmail.com>
2025-10-22 11:44:06 +0530

    db.js changed

--- config/db.js
+++ config/db.js
@@ -8,6 +8,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
